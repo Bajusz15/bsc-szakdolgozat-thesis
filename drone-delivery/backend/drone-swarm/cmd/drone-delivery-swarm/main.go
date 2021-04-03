f9732715a68eadae97979bebf0a3f2125992d367
Máté Bajusz <bajszmate99@gmail.com>
2021-04-03 12:10:21 +0200

    feat: renaming packages + go kit logger

--- drone-delivery/backend/drone-swarm/cmd/drone-delivery-swarm/main.go
+++ drone-delivery/backend/drone-swarm/cmd/drone-delivery-swarm/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"drone-delivery/drone-swarm/pkg/domain/flying"
+	"drone-delivery/drone-swarm/pkg/domain/routing"
+	"drone-delivery/drone-swarm/pkg/domain/telemetry"
+	"drone-delivery/drone-swarm/pkg/network/inbound/http/rest"
 	"fmt"
-	"github.com/bajusz15/drone-delivery/drone-swarm/pkg/network/inbound/http/rest"
+	"github.com/StefanSchroeder/Golang-Ellipsoid/ellipsoid"
+	goKitLog "github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("ez a raktár a szimulacioban, a raktár és drónok példányait szimulálja.")
+	var logger goKitLog.Logger
+	logger = goKitLog.NewLogfmtLogger(os.Stderr)
+	logger = level.NewFilter(logger, level.AllowInfo()) // <--
+	logger = goKitLog.With(logger, "ts", goKitLog.DefaultTimestampUTC)
+
+	telemetryService := telemetry.NewService()
+	geo := ellipsoid.Init("WGS84", ellipsoid.Degrees, ellipsoid.Meter, ellipsoid.LongitudeIsSymmetric, ellipsoid.BearingIsSymmetric)
+	routingService := routing.NewService(geo)
+	flying.NewService(telemetryService, routingService, logger)
 	log.Fatal(http.ListenAndServe(":2000", rest.Handler()))
 }
 
